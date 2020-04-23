84986791605a4f359c1152937b65c33f6bc78529
jesseyowell <jesse.yowell@gmail.com>
2020-04-23 11:02:46 -0700

    fun with putchar

---
+++ putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/* Let's get characters as long there is no EOF (newlines don't count as EOF) */
+/* We're going to output the values via putchar(), and also print EOF's value and the expression we created to evaluate if c does not equal EOF! */
+
+int main() {
+
+	int c;
+
+	while ((c = getchar()) != EOF) {
+		printf("%d\n",EOF);
+		printf("%d\n",(c = getchar()) != EOF);
+		putchar(c);
+	}
+
+	return EOF;
+}
