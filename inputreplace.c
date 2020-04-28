53cbdddc832a0eeb41ca3d585c7f770e25f9ab49
jesseyowell <jesse.yowell@gmail.com>
2020-04-28 15:48:07 -0700

    copy / paste input with replacements

---
+++ inputreplace.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/* Simple input to output copier */
+/* Replaces tabs with '\t', blank space with '\s', and backslash with '\\' */
+/* Backslash requires double slash (//) in getchar then quadruple (////) to be printed */
+
+int main() {
+    int c;
+
+	while ((c = getchar()) != EOF) {
+		if(c == '\t') {
+			while((c = getchar()) == '\t')
+			;	
+		  printf("\\t");
+	    }
+	    if(c == ' ') {
+	    	while((c = getchar()) == ' ')
+	    	;
+	      printf("\\b");
+	    }
+	    if(c == '\\') {
+	    	while((c = getchar()) == '\\')
+	        ;
+	      printf("\\\\");  		
+	    }
+		putchar(c);
+	}
+	return 0;
+}
