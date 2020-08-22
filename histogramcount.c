3bb99cc2334139fbebcff39c46a3d4a0c861db13
jesseyowell <jesse.yowell@gmail.com>
2020-08-21 22:03:32 -0700

    fixed this goddamn thing!

--- histogramcount.c
+++ histogramcount.c
@@ -6,35 +6,33 @@
 /* count words entered */
 
 int main() {
-	int c, i, state, count, addnum;
+	int c, i, j, state, count;
 	int nchar[10];
 	count = 0;
-	addnum = 0;
 	state = IN;
 
-	for(int i = 0; i < 10; ++i) {
+	for(i = 0; i < 10; ++i) {
 		nchar[i] = 0;
 	}
 
 	while ((c = getchar()) != EOF) {
        
         if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
-			++count;
-		 }   
-	     else if(c == ' ' || c == '\n' || c == '\t' || c == '\r') {
-			state = OUT;
-			nchar[count] = ++addnum;
-			printf("%d",count);
-			count = 0;  // this resets the char count, for the next word.
-		 }
+		  ++count;
+		}   
+	    else if(c == ' ' || c == '\n' || c == '\t' || c == '\r') {
+		  ++nchar[count];
+		  state = OUT;
+		  count = 0; // this resets the char count, for the next word
+		}
 		else state = IN;
-	
-	 }  
-        printf("word size = ");
+	}  
+       printf("word size = ");
 	      
-	      for(int j = 1; j < 10; ++j) {
-	    	printf("%d", nchar[j]);       // prints out ten digits, that will correspond to the size of each word entered.
-	      }
+	    for(j = 1; j < 10; ++j) {
+	      printf("%d", nchar[j]);  // prints out ten digits that will correspond to the size of each word entered. (004) = 4 3-letter words,
+	                               // we add 1 to j so we don't print out 0 char words
+	    }
 		putchar('\n');
 
 		return 0;		
